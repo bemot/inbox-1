48efe60470acbcb62c2507e75ffa195ac68af8b6
Ian Hecker <ianhecker@yahoo.com>
2019-05-07 16:39:16 -0600

    removed bat file

--- saveprogress.bat
+++
@@ -1,9 +0,0 @@
-git add * 
-git status
-@echo off
-set /p input="would you like to commit additions? y/n: "
-IF /I "%input%" == "y" (
-    set /p commit_message="enter a commit message: "    
-    git commit -m echo %commit_message%
-    
-)
