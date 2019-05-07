d503512ef4ebdd2c612a0364ab4b2b8d6995d3fa
Ian Hecker <ianhecker@yahoo.com>
2019-05-07 12:22:49 -0600

    documentation

--- README.md
+++ README.md
@@ -1,9 +1,47 @@
 ### Testing
 ```
-npm run test
+$ npm run test
 ```
+#### Mocha
+General purpose test tool
++ Functions: 
+    + it -> run test and make an assertion
+    + describe -> groups together 'it' functions
+    + beforeEach -> execute some general setup code
+```javascript
+ class Car {
+     park() {return 'stopped'}
+     drive() {return 'vroom'}
+ }
+```
+#### mocha test formating
++ describe(first arg -> organization only, second arg -> writing 'it' tests)
+    + houses 'it' functions and their assertions
++ it(first arg -> 'title of test', second arg -> assert tests)
+    + used to assert tests
++ beforeEach(first arg -> arrow function)
+    + will perform arrow function before any 'it' statement
+```javascript
+//init var car for 'it' scopes
+let car;
+//init car instance for 'it' statements
+beforeEach(() => {
+    car = new Car()
+})
 
+describe('Car', () => {
+    //Test method park()
+    it('can park', () => {                
+        assert.equal(car.park(), 'stopped')
+    })
+
+    //Test method drive()
+    it('can drive', () => {           
+        assert.equal(car.drive(), 'vroom')
+    })
+})
+```
 ### Compile
 ```
-node compile.js
+$ node compile.js
 ```
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,25 +6,42 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
 
+
+
+
+
+//##################################
 //Mocha -> general purpose test tool
 //Functions: 
 //it -> run test and make an assertion
 //describe -> groups together 'it' functions
 //beforeEach -> execute some general setup code
 
-class Car {
-    park() {return 'stopped'}
-    drive() {return 'vroom'}
-}
+// class Car {
+//     park() {return 'stopped'}
+//     drive() {return 'vroom'}
+// }
 
+//***mocha test formating***
+//describe(first arg -> organization only, second arg -> writing 'it' tests)
+//it(first arg -> 'title of test', second arg -> assert tests)
+//beforeEach(first arg -> arrow function) //will perform arrow function before any 'it' statement
 
-//mocha test format
-//first arg -> organization only
-//second arg -> writing tests
-describe('Car', () => {
-    it('can park', () => {
-        const car = new Car()
-        //call instance method park, assert that it equals 'stopped'
-        assert.equal(car.park(), 'stopped')
-    })
-})
+//init var car for 'it' scopes
+// let car;
+// //init car instance for 'it' statements
+// beforeEach(() => {
+//     car = new Car()
+// })
+
+// describe('Car', () => {
+//     //Test method park()
+//     it('can park', () => {                
+//         assert.equal(car.park(), 'stopped')
+//     })
+
+//     //Test method drive()
+//     it('can drive', () => {           
+//         assert.equal(car.drive(), 'vroom')
+//     })
+// })
