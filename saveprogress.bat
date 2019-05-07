a7434b8abd4220054e22549896a42b213942f30d
Ian Hecker <ianhecker@yahoo.com>
2019-05-07 16:34:27 -0600

    echo

--- saveprogress.bat
+++ saveprogress.bat
@@ -4,6 +4,6 @@ git status
 set /p input="would you like to commit additions? y/n: "
 IF /I "%input%" == "y" (
     set /p commit_message="enter a commit message: "    
-    git commit -m "hey"
+    git commit -m echo %commit_message%
     
 )
